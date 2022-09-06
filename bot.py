20546b878a079b0f810305ffca51ba74a1a212f6
Xrems <75927568+Xrems@users.noreply.github.com>
2022-09-06 19:14:43 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,220 @@
+from aiogram import Bot, types
+from aiogram.dispatcher import Dispatcher
+from aiogram.utils import executor
+from config import TOKEN
+import keyboards as kb
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher(bot)
+
+# [](tg://user?id=chat_id)
+
+# первая страница списка студентов
+first_half = (f"🧑‍🎓1.  Бичков Михайло \n"   
+        f"🧑‍🎓2.  Білоусов Дмитро\n"   
+        f"🧑‍🎓3.  Богданов Олексій\n" 
+        f"👩‍🎓4.  Бочарова Вікторія\n"     
+        f"🧑‍🎓5.  Жукотський Микита\n"   
+        f"🧑‍🎓6.  Іванченко Богдан\n"   
+        f"🧑‍🎓7.  Ісаєнко  Гліб\n"  
+        f"👩‍🎓8.  Колесник Олександра\n"   
+        f"🧑‍🎓9.  Кудиненко Кирило\n"    
+        f"🧑‍🎓10.  Курило   Ярослав\n"  
+        f"🧑‍🎓11.  Мокренко Костянтин\n"    
+        f"🧑‍🎓12.  Мурадян Арсеній\n"   
+        f"👩‍🎓13.  Остапенко Анжела")  
+
+# вторая страница списка студентов
+second_half = (f"🧑‍🎓14.  Петрик Олександр\n"
+            f"🧑‍🎓15.  Печеньов Артем\n"
+            f"🧑‍🎓16.  Полянський Руслан\n" 
+            f"🧑‍🎓17.  Прокудін Ігор\n"
+            f"🧑‍🎓18.  Пряницький Олексій\n"
+            f"🧑‍🎓19.  Рало Ілля\n"
+            f"👩‍🎓20.  Рябовол Диана\n"
+            f"🧑‍🎓21.  Страхов  Ігор\n"
+            f"🧑‍🎓22.  Сударев Игорь\n"
+            f"🧑‍🎓23.  Тарсуков Данило\n"
+            f"👩‍🎓24.  Царькова Дарина\n"
+            f"🧑‍🎓25.  Шахов Кирило\n"
+            f"🧑‍🎓26.  Шеховцов Роман\n"
+            f"🧑‍🎓27.  Шимкевич Ярослав")
+
+@dp.message_handler(content_types='text')
+async def forward(message: types.Message):
+    #реакция на /start
+    if message.text == "/start":
+        await message.reply(f"{message.from_user.first_name}, я предназначен только для группы <pre>ПЗ-11</pre>"
+                            f"\nХочешь узнать расписание? Напиши <code>!пары</code>, и я вышлю тебе фото."
+                            f"\n\nТак же я умею пересылать ссылку на пары из одной группы в другую и закреплять сообщение там."
+                            f"\nВесь функционал бота можно узнать по команде /help"
+                            f"\n---\n👨‍💻 Код писали: \n<a href='tg://user?id=1051198514'>🔸Игорь</a>"
+                            f"\n<a href='tg://user?id=562813685'>🔸Арсен</a>" , parse_mode='HTML')
+
+    # узнать свой id и id группы
+    if message.text == '/id'  or message.text == "!id"  or message.text == "!ид"  or message.text == "/id@pz11_bot":
+        await message.reply(f"Твой id: {message.from_user.id}\nЧат id: `{message.chat.id}`", parse_mode="MarkdownV2")
+
+    # вызов рассписания
+    if message.text == '!пары' or message.text=="/pr" or message.text == "/pr@pz11_bot":
+        await message.answer_photo("https://telegra.ph/file/6f4cbad77f99e7fa810ea.png", reply_markup=kb.pn2)
+
+    # вызов списка стедентов
+    if message.text == '!группа' or message.text=="!студенты" or message.text=="/students" or message.text == "/students@pz11_bot":
+        # first_half = (f"🧑‍🎓1.  Бичков Михайло \n"   
+        # f"🧑‍🎓2.  Білоусов Дмитро\n"   
+        # f"🧑‍🎓3.  Богданов Олексій\n" 
+        # f"👩‍🎓4.  Бочарова Вікторія\n"     
+        # f"🧑‍🎓5.  Жукотський Микита\n"   
+        # f"🧑‍🎓6.  Іванченко Богдан\n"   
+        # f"🧑‍🎓7.  Ісаєнко  Гліб\n"  
+        # f"👩‍🎓8.  Колесник Олександра\n"   
+        # f"🧑‍🎓9.  Кудиненко Кирило\n"    
+        # f"🧑‍🎓10.  Курило   Ярослав\n"  
+        # f"🧑‍🎓11.  Мокренко Костянтин\n"    
+        # f"🧑‍🎓12.  Мурадян Арсеній\n"   
+        # f"👩‍🎓13.  Остапенко Анжела")  
+        await message.reply(first_half, reply_markup=kb.fisrt_page_button)
+        
+    # вызвать список кодеров
+    if message.text == '!админы' or message.text == "!разработчики" or message.text == "/adm"or message.text == "/adm@pz11_bot":
+        await message.reply("👨‍💻 Код писали:"
+                            "\n <a href='tg://user?id=1051198514'>🔸Страхов Игорь</a>"
+                            "\n <a href='tg://user?id=562813685'>🔸Мурадян Арсен</a>" , parse_mode='HTML')
+
+    # проверить отклик бота
+    if message.text == "!бот" or message.text == "!ботик" or message.text == "/bot@pz11_bot":
+        await message.reply("На месте")
+
+    # рассписание звонков
+    if message.text == "!звонки" or message.text == "/call" or message.text == "/call@pz11_bot":
+        await message.reply(f"{message.from_user.first_name}, расписание звонков:"
+                            f"\n1️⃣ пара: 8-30. 9-50"
+                            f"\n2️⃣ пара: 10.00 11-20"
+                            f"\n3️⃣ пара: 11-50  13-10"
+                            f"\n4️⃣ пара: 13-20. 14-40")
+
+    # переслать ссылки на зум и гугл мит
+    if message.chat.id != -1001723681888: 
+        if 'zoom.us' in message.text:
+            message_pin = await bot.send_message(-1001723681888, message.text + '\nСсылка на Zoom из группы: ' + message.chat.title)
+            await bot.send_message(message.chat.id, 'я отправил ссылку студентам ✅')
+            await message_pin.pin(False)
+    if message.chat.id != -1001723681888: 
+        if 'meet.google.com' in message.text:
+            message_pin = await bot.send_message(-1001723681888, message.text + '\nСсылка на Google meet из группы: ' + message.chat.title)
+            await message_pin.pin(False)
+
+
+    # команда !помощь
+    if message.text == '!помощь' or message.text == "/help" or message.text == "/help@pz11_bot":
+        await message.reply(f'{message.from_user.first_name}, все команды указываются через !название команды или /название команды\n'
+                            f"----------\n"
+                            f'Список команд:\n'
+                            f'🆔 !id | /!id - Показывает ваш id и id группы.\n'
+                            f'🗓 !пары | /pr - Выдает меню где можно посмотреть расписание.\n'
+                            f'🔔 !звонки | /call - Показывает расписание звонков.\n'
+                            f'🎓 !студенты | /students - Список стдентов в группе ПЗ - 11.\n'
+                            f'🎓 !фио | /fio - Показывает ФИО преподавателей\n'
+                            f'👨‍💻 !разработчики | /adm - Показывает кто разработал этого бота.\n \n'
+                            f'Если у вас есть предложения по улучшению бота-обращайтесь к разработчикам.')
+    # команда ФИО
+    if message.text == '/fio':
+        await message.answer('Выбери:', reply_markup=kb.all_button)
+    
+
+
+@dp.callback_query_handler()
+async def button(query: types.CallbackQuery):
+    if query.data == 'pn':
+        media = types.InputMediaPhoto('https://telegra.ph/file/6f4cbad77f99e7fa810ea.png')
+        await bot.edit_message_media(media, query.message.chat.id, query.message.message_id, reply_markup=kb.pn2)
+    if query.data == 'vt':
+        media = types.InputMediaPhoto('https://telegra.ph/file/81ab8b5b475dcb00798ff.png')
+        await bot.edit_message_media(media, query.message.chat.id, query.message.message_id, reply_markup=kb.vt2)
+    if query.data == 'sr':
+        media = types.InputMediaPhoto('https://telegra.ph/file/82ae56b5c61854dbd5ef5.png')
+        await bot.edit_message_media(media, query.message.chat.id, query.message.message_id, reply_markup=kb.sr2)
+    if query.data == 'cht':
+        media = types.InputMediaPhoto('https://telegra.ph/file/97a0f47b56f0abc7451ff.png')
+        await bot.edit_message_media(media, query.message.chat.id, query.message.message_id, reply_markup=kb.cht2)
+    if query.data == 'pt':
+        media = types.InputMediaPhoto('https://telegra.ph/file/d4118ae8cbd5c10306a50.png')
+        await bot.edit_message_media(media, query.message.chat.id, query.message.message_id, reply_markup=kb.pt2)
+
+    # список студентов
+    if query.data == 'next_page_data':      
+        await bot.edit_message_text(second_half, query.message.chat.id, query.message.message_id, reply_markup=kb.second_page_button)
+
+    if query.data == 'back_page_data':
+        # first_page = (f"🧑‍🎓1.  Бичков Михайло \n"   
+        # f"🧑‍🎓2.  Білоусов Дмитро\n"   
+        # f"🧑‍🎓3.  Богданов Олексій\n" 
+        # f"👩‍🎓4.  Бочарова Вікторія\n"     
+        # f"🧑‍🎓5.  Жукотський Микита\n"   
+        # f"🧑‍🎓6.  Іванченко Богдан\n"   
+        # f"🧑‍🎓7.  Ісаєнко  Гліб\n"  
+        # f"👩‍🎓8.  Колесник Олександра\n"   
+        # f"🧑‍🎓9.  Кудиненко Кирило\n"    
+        # f"🧑‍🎓10.  Курило   Ярослав\n"  
+        # f"🧑‍🎓11.  Мокренко Костянтин\n"    
+        # f"🧑‍🎓12.  Мурадян Арсеній\n"   
+        # f"👩‍🎓13.  Остапенко Анжела")
+        await bot.edit_message_text(first_half, query.message.chat.id, query.message.message_id, reply_markup=kb.fisrt1_page_button)     
+
+    # кнопки с фио преподавателей
+    if query.data == 'math_data':
+        await bot.edit_message_text('Математика: Матвейченко Елена Владимировна', query.message.chat.id,
+                                    query.message.message_id, reply_markup=kb.all_button)
+    if query.data == 'fizra_data':
+        await bot.edit_message_text('Физра: Малик Инна Николаевна', query.message.chat.id, query.message.message_id,
+                                    reply_markup=kb.all_button)
+
+    if query.data == 'history_data':
+        await bot.edit_message_text('История Украины: Гуленко Людмила Филипповна', query.message.chat.id, query.message.message_id,
+                                    reply_markup=kb.all_button)
+
+    if query.data == 'eng_data':
+        await bot.edit_message_text('Английский:\nИгнатьева  Наталия Викторовна\n'
+                                    'Каташов Олександр Анатольевич.', query.message.chat.id, query.message.message_id,
+                                    reply_markup=kb.all_button)
+
+    if query.data == 'ua_data':
+        await bot.edit_message_text('Укр яз и лит: Волкова Лилия Владимировна.', query.message.chat.id, query.message.message_id,
+                                    reply_markup=kb.all_button)
+
+    if query.data == 'ph_data':
+        await bot.edit_message_text('Физика:\nСеменченко Татьяна Александровна.\n'
+                                    'Корнеева Ирина Анатаоливна', query.message.chat.id, query.message.message_id,
+                                    reply_markup=kb.all_button)
+
+    if query.data == 'BJD_data':
+        await bot.edit_message_text('БЖД: Мирошиченко Юрий Викторович.', query.message.chat.id, query.message.message_id,
+                                    reply_markup=kb.all_button)
+
+    if query.data == 'info_data':
+        await bot.edit_message_text('Инф.технологии: Беликова Виктория Викторовна.', query.message.chat.id, query.message.message_id,
+                                    reply_markup=kb.all_button)
+
+    if query.data == 'gr_data':
+        await bot.edit_message_text('Гром.освита: Оксененко Светлана Петровна', query.message.chat.id, query.message.message_id,
+                                    reply_markup=kb.all_button)
+
+    if query.data == 'tech_data':
+        await bot.edit_message_text('Технологии: Милютина Ольга Святославовна', query.message.chat.id, query.message.message_id,
+                                    reply_markup=kb.all_button)
+
+    if query.data == 'tks_data':
+        await bot.edit_message_text('ОС ТКС: Качуров Вячеслав Евгеньевич', query.message.chat.id, query.message.message_id,
+                                    reply_markup=kb.all_button)
+
+    if query.data == 'bio_data':
+        await bot.edit_message_text('Биология: Каплун Елена Анатольевна', query.message.chat.id, query.message.message_id,
+                                    reply_markup=kb.all_button)
+
+    if query.data == 'ect_data':
+        await bot.edit_message_text('ТЭЦ: Беликова Станислава Олеговна', query.message.chat.id, query.message.message_id,
+                                    reply_markup=kb.all_button)
+
+if __name__ == '__main__':
+    executor.start_polling(dp)
---
+++ keyboards.py
@@ -0,0 +1,59 @@
+from aiogram.types import ReplyKeyboardRemove, \
+    ReplyKeyboardMarkup, KeyboardButton, \
+    InlineKeyboardMarkup, InlineKeyboardButton
+
+
+button_hi = KeyboardButton('/shn')
+
+pn = InlineKeyboardButton('Понедельник', callback_data='pn')
+vt = InlineKeyboardButton('Вторник', callback_data='vt')
+sr = InlineKeyboardButton('Среда', callback_data='sr')
+cht = InlineKeyboardButton('Четверг', callback_data='cht')
+pt = InlineKeyboardButton('Пятница', callback_data='pt')
+
+#c ромбиком
+pn1 = InlineKeyboardButton('🔹Понедельник', callback_data='pn')
+vt1 = InlineKeyboardButton('🔹Вторник', callback_data='vt')
+sr1 = InlineKeyboardButton('🔹Среда', callback_data='sr')
+cht1 = InlineKeyboardButton('🔹Четверг', callback_data='cht')
+pt1 = InlineKeyboardButton('🔹Пятница', callback_data='pt')
+
+pn2 = InlineKeyboardMarkup().add(pn1, vt, sr, cht, pt)
+vt2 = InlineKeyboardMarkup().add(pn, vt1, sr, cht, pt)
+sr2 = InlineKeyboardMarkup().add(pn, vt, sr1, cht, pt)
+cht2 = InlineKeyboardMarkup().add(pn, vt, sr, cht1, pt)
+pt2 = InlineKeyboardMarkup().add(pn, vt, sr, cht, pt1)
+
+# Список студентов
+page = InlineKeyboardButton("Страница: ", callback_data="page")
+first_page = InlineKeyboardButton("1/2", callback_data="first_page_data")
+second_page = InlineKeyboardButton("2/2", callback_data="second_page_data")
+back_page_first = InlineKeyboardButton("Назад ⬅️", callback_data="back_page_data")
+next_page_two = InlineKeyboardButton("Дальше ▶️", callback_data="next_page_data")
+fisrt_page_button = InlineKeyboardMarkup().add(page, first_page).add(next_page_two)
+fisrt1_page_button = InlineKeyboardMarkup().add(page, first_page).add(next_page_two)
+second_page_button = InlineKeyboardMarkup().add(page, second_page).add(back_page_first)
+
+# Название предметов
+math = InlineKeyboardButton('Математика', callback_data='math_data')
+fizra = InlineKeyboardButton('Физ-ра', callback_data='fizra_data')
+history = InlineKeyboardButton('История Украины', callback_data='history_data')
+eng = InlineKeyboardButton('Англ.яз', callback_data='eng_data')
+ua = InlineKeyboardButton('Украинский язык и литература', callback_data='ua_data')
+physics = InlineKeyboardButton('Физика', callback_data='ph_data')
+BJD = InlineKeyboardButton('БЖД', callback_data='BJD_data')
+info = InlineKeyboardButton('Инф.технологии', callback_data='info_data')
+gr = InlineKeyboardButton('Гром.Освита', callback_data='gr_data')
+Tech = InlineKeyboardButton('Технологии', callback_data='tech_data')
+TKS = InlineKeyboardButton('ОС ТКС', callback_data='tks_data')
+Biology = InlineKeyboardButton('Биология', callback_data='bio_data')
+ECT = InlineKeyboardButton('ТЭЦ', callback_data='ect_data')
+
+# Создаем клавиатуру кнопок
+all_button = InlineKeyboardMarkup().add(math, physics).\
+    add(history, info)\
+    .add(ua)\
+    .add(fizra, BJD, eng)\
+    .add(gr, Tech)\
+    .add(TKS, ECT)\
+    .add(Biology)
